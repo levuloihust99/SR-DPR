6eaa1d2ab2229107c2f3bb2222c3c5c227942e46
NamDo <namdohai.hust@gmail.com>
2023-07-24 06:55:47 +0000

    Update setup.py transformers--version --> 4.12.5

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         'numpy',
         'regex',
         'torch>=1.6.0',
-        'transformers>=3.0.0,<3.1.0',
+        'transformers==4.12.5',
         'tqdm>=4.27',
         'wget',
         'spacy>=2.1.8',
